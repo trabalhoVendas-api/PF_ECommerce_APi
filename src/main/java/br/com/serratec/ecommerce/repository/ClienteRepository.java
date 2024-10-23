597145e1e7f207eb5e82c2afbaa8638ef4b3c33a
thaissan <thais.cavadas@hotmail.com>
2024-10-23 20:45:45 -0300

    avaliacao

--- src/main/java/br/com/serratec/ecommerce/repository/ClienteRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.serratec.ecommerce.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.com.serratec.ecommerce.entity.Cliente;
-
-public interface ClienteRepository extends JpaRepository<Cliente, Long>{
-
-}
