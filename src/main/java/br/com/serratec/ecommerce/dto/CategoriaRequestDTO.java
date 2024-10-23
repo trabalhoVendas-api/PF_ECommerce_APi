a967806e762fe734ec40a4e35d5430e10aef9f8f
thaissan <thais.cavadas@hotmail.com>
2024-10-22 22:10:34 -0300

    Adicionado service, controller e dto de Categoria

---
+++ src/main/java/br/com/serratec/ecommerce/dto/CategoriaRequestDTO.java
@@ -0,0 +1,5 @@
+package br.com.serratec.ecommerce.dto;
+
+public class CategoriaRequestDTO {
+
+}
---
+++ src/main/java/br/com/serratec/ecommerce/repository/CategoriaRepository.java
@@ -0,0 +1,5 @@
+package br.com.serratec.ecommerce.repository;
+
+public class CategoriaRepository {
+
+}
