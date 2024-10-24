e48b06ca913be4dab355990e98aade0b2c226e5b
thaissan <thais.cavadas@hotmail.com>
2024-10-23 21:25:37 -0300

    Avaliacao de produtos adicionada

---
+++ src/main/java/br/com/serratec/ecommerce/dto/AvaliacaoDTO.java
@@ -0,0 +1,73 @@
+package br.com.serratec.ecommerce.dto;
+
+import java.time.LocalDateTime;
+
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+public class AvaliacaoDTO {
+    private Long id;
+    private Long produtoId;
+    private Long clienteId;
+
+    @NotNull(message = "A nota deve estar entre 1 e 5")
+    @Min(1)
+    @Max(5)
+    private Integer nota;
+
+    @Size(max = 1000)
+    private String comentario;
+
+    private LocalDateTime dataAvaliacao;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Long getProdutoId() {
+		return produtoId;
+	}
+
+	public void setProdutoId(Long produtoId) {
+		this.produtoId = produtoId;
+	}
+
+	public Long getClienteId() {
+		return clienteId;
+	}
+
+	public void setClienteId(Long clienteId) {
+		this.clienteId = clienteId;
+	}
+
+	public Integer getNota() {
+		return nota;
+	}
+
+	public void setNota(Integer nota) {
+		this.nota = nota;
+	}
+
+	public String getComentario() {
+		return comentario;
+	}
+
+	public void setComentario(String comentario) {
+		this.comentario = comentario;
+	}
+
+	public LocalDateTime getDataAvaliacao() {
+		return dataAvaliacao;
+	}
+
+	public void setDataAvaliacao(LocalDateTime dataAvaliacao) {
+		this.dataAvaliacao = dataAvaliacao;
+	}
+
+}
